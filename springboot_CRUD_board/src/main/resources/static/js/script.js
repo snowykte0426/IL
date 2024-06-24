1ed1a382af08665baed311b9dee4b73d12e184b9
KIM TAEEUN <snowykte0426@naver.com>
2024-06-24 22:30:28 +0900

    미사용 Java 파일 삭제

--- springboot_CRUD_board/src/main/resources/static/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var message = window.message || 'default message';
-    var searchUrl = window.searchUrl || 'defaultSearchUrl';
-    alert(message);
-    location.replace(searchUrl);
-});
